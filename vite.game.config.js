41aceaaea8cc4808ae059212ef73af5ae2584f6c
bigknoxy <bigknoxy@users.noreply.github.com>
2025-12-05 13:13:56 -0600

    chore(deploy): ensure game bundle and styles included in Pages deployment

--- vite.game.config.js
+++ vite.game.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  publicDir: false, // Don't copy public dir assets
   build: {
     lib: {
       entry: "./src/game/index.js",
@@ -12,6 +13,7 @@ export default defineConfig({
       external: [],
       output: {
         dir: "./dist/game",
+        entryFileNames: "game-engine.js", // Fixed filename for predictable URL
       },
     },
   },
